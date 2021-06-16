c9193caad6516750c1c3ce63c414185a87ed1068
Kompic <Kompic@DESKTOP-DSFML31>
2021-06-16 14:18:55 +0200

    api gateway added

---
+++ naming-server/src/test/java/com/XMLiWS/microservices/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.XMLiWS.microservices.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
